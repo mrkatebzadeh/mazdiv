7714823427f6c3d07f400ecb56e57cfb49100a13
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2024-10-19 17:00:21 +0100

    Added padding to neovide

--- init.lua
+++ init.lua
@@ -1,24 +1,28 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("vim-options")
 -- require("lsp-manager")
 require("lazy").setup("plugins", {
-	ui = {
-		size = { width = 0.9, height = 0.4 },
-		border = "rounded",
-	},
+  ui = {
+    size = { width = 0.9, height = 0.4 },
+    border = "rounded",
+  },
 })
 require("bindings")
 require("autocommands")
 require("utils")
+
+if vim.g.neovide then
+  require("neovide")
+end
---
+++ lua/neovide.lua
@@ -0,0 +1,9 @@
+vim.g.neovide_padding_top = 15
+vim.g.neovide_padding_bottom = 0
+vim.g.neovide_padding_right = 10
+vim.g.neovide_padding_left = 10
+
+vim.g.neovide_transparency = 0.8
+vim.g.neovide_window_blurred = true
+vim.g.neovide_floating_blur_amount_x = 2.0
+vim.g.neovide_floating_blur_amount_y = 2.0
