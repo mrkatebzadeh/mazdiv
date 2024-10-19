fd0b72209f17f5c73c93f2a6c3ecad8d5ab6212d
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2024-10-19 10:38:15 +0100

    Added oil

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,13 @@
+return {
+  "stevearc/oil.nvim",
+  opts = {
+    default_file_explorer = true,
+    columns = {
+      "icon",
+      "permissions",
+      "size",
+      "mtime",
+    },
+  },
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+}
