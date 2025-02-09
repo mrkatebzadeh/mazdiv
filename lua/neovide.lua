149c8fd0cb19cb37513f2bee798d49a9bdf040dc
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2025-02-09 23:52:34 +0000

    Fixed pasting in neovide

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -11,3 +11,9 @@ vim.g.neovide_floating_blur_amount_y = 2.0
 local default_path = vim.fn.expand("~")
 vim.api.nvim_set_current_dir(default_path)
 vim.o.guifont = "FiraCode Nerd Font Propo:h12"
+
+vim.api.nvim_set_keymap('', '<D-v>', '+p<CR>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('t', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('v', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('t', '<D-v>', '<C-\\><C-o>"+p', { noremap = true, silent = true })
