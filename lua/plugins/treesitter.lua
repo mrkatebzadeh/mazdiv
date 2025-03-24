8977ec4b9a00e150a5c82e8e0c6c4a60ee69716c
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2025-03-24 01:59:29 +0000

    Added nu

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,22 +1,25 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-    version = "v0.9.2",
-		build = ":TSUpdate",
-		config = function()
-			local config = require("nvim-treesitter.configs")
-			config.setup({
-				ensure_installed = { "c", "lua", "rust", "bash", "vim", "html", "bash" },
-				auto_install = true,
-				highlight = { enable = true },
-				indent = { enable = true },
-				autotag = {
-					enable = true,
-				},
-			})
-		end,
-	},
   {
-    "windwp/nvim-ts-autotag"
-  }
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+      { "nushell/tree-sitter-nu" },
+    },
+    version = "v0.9.3",
+    build = ":TSUpdate",
+    config = function()
+      local config = require("nvim-treesitter.configs")
+      config.setup({
+        ensure_installed = { "c", "lua", "rust", "vim", "html", "bash" },
+        auto_install = true,
+        highlight = { enable = true },
+        indent = { enable = true },
+        autotag = {
+          enable = true,
+        },
+      })
+    end,
+  },
+  {
+    "windwp/nvim-ts-autotag",
+  },
 }
