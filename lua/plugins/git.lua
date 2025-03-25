bf7db3967a2dfbcf91dce62cbd9f913ed02b1f20
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2025-03-25 17:55:59 +0000

    Updated packages

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,23 +1,33 @@
 return {
-	{
-		"kdheepak/lazygit.nvim",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
-	},
-	{
-		"lewis6991/gitsigns.nvim",
-		config = function()
-			require("gitsigns").setup()
-		end,
-	},
-	{
-		"NeogitOrg/neogit",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"sindrets/diffview.nvim",
-			"nvim-telescope/telescope.nvim",
-		},
-		config = true,
-	},
+  {
+    "kdheepak/lazygit.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup()
+    end,
+  },
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    config = function()
+      local neogit = require("neogit")
+      neogit.setup({
+        commit_editor = {
+          kind = "tab",
+          show_staged_diff = true,
+          staged_diff_split_kind = "auto",
+          spell_check = true,
+        },
+      })
+    end,
+  },
 }
