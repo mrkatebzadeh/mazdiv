86053fb7a5c7662ec791bfa38554d1a2dca5d7b3
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2025-05-26 16:57:14 +0100

    Added make and meson treesitters

--- lua/plugins/langs/cpp.lua
+++ lua/plugins/langs/cpp.lua
@@ -1,6 +1,44 @@
+--[[ cpp.lua
+
+Author: M.R. Siavash Katebzadeh <mr@katebzadeh.xyz>
+Keywords: Lua, Neovim
+Version: 0.0.1
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+]]
 return {
-  "madskjeldgaard/cppman.nvim",
-  dependencies = {
-    { "MunifTanjim/nui.nvim" },
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = { "make", "meson" },
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			servers = {
+				clangd = {
+					mason = false,
+				},
+			},
+		},
+	},
+	{
+		"madskjeldgaard/cppman.nvim",
+		dependencies = {
+			{ "MunifTanjim/nui.nvim" },
+		},
+	},
 }
+--[[ cpp.lua ends here. ]]
