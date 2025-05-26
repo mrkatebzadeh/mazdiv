dd4624acdd4e83f7da6aa034090bcbee981e2a88
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2025-05-26 02:00:32 +0100

    Updated comments

--- lua/bindings/comment.lua
+++ lua/bindings/comment.lua
@@ -6,29 +6,17 @@ COMMENT.setup = function()
 		return
 	end
 
-	local K = vim.keymap.set
-	-- K("n", "K", vim.lsp.buf.hover, {})
-
-	local call = require("Comment.api").call
-
-	K("n", "<leader>/", call("toggle.linewise.current", "g@$"), { expr = true, desc = "Comment toggle current line" })
-
-	K(
-		"x",
-		"<leader>/",
-		'<ESC><CMD>lua require("Comment.api").locked("toggle.linewise")(vim.fn.visualmode())<CR>',
-		{ desc = "Comment toggle linewise (visual)" }
-	)
-
-	K(
-		"x",
-		"<leader>/",
-		'<ESC><CMD>lua require("Comment.api").locked("toggle.blockwise")(vim.fn.visualmode())<CR>',
-		{ desc = "Comment toggle blockwise (visual)" }
-	)
-
 	local mappings = {
-		{ "<leader>/", group = "Window", mode = { "v", "n" }, nowait = true, remap = false },
+		mode = { "v", "n" },
+		{
+			"<leader>/",
+			function()
+				vim.cmd.norm("gcc")
+			end,
+			desc = "Comment",
+			nowait = true,
+			remap = false,
+		},
 	}
 	which_key.add(mappings)
 end
