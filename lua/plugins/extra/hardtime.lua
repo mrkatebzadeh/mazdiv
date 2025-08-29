2c3b3322617cab9a00730ed16f0cc6a45ad921be
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2025-08-29 22:45:02 +0100

    Disabled hardtime

--- lua/plugins/extra/hardtime.lua
+++ lua/plugins/extra/hardtime.lua
@@ -1,13 +1,14 @@
 return {
-	{
-		"m4xshen/hardtime.nvim",
-		dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-		opts = {},
-		config = function()
-			require("hardtime").setup({
-				disabled_filetypes = { "qf", "NeoTree", "netrw", "NvimTree", "lazy", "mason", "oil" },
-				max_count = 10,
-			})
-		end,
-	},
+  {
+    "m4xshen/hardtime.nvim",
+    enabled = false,
+    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+    opts = {},
+    config = function()
+      require("hardtime").setup({
+        disabled_filetypes = { "qf", "NeoTree", "netrw", "NvimTree", "lazy", "mason", "oil" },
+        max_count = 10,
+      })
+    end,
+  },
 }
