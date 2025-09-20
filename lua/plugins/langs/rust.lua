ef814cc84275e26d67aabd64b2c2cbb6928757c6
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2025-09-20 12:54:01 +0100

    Cleaned

--- lua/plugins/langs/rust.lua
+++ lua/plugins/langs/rust.lua
@@ -1,65 +1,52 @@
 return {
-	{
-		"mrcjkb/rustaceanvim",
-		dependencies = {
-			-- "lvimuser/lsp-inlayhints.nvim",
-			-- "ray-x/lsp_signature.nvim",
-			-- opts = {},
-		},
-		version = "6.9.1",
-		ft = { "rust" },
-		config = function()
-			local mason_registry = require("mason-registry")
-			local codelldb = mason_registry.get_package("codelldb")
-			local extension_path = codelldb:get_install_path() .. "/extension/"
-			local codelldb_path = extension_path .. "adapter/codelldb"
-			local liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
-			vim.g.rustaceanvim = {
-				inlay_hints = {
-					auto = true,
-					only_current_line = false,
-					show_parameter_hints = true,
-					parameter_hints_prefix = "<-",
-					other_hints_prefix = "=> ",
-					max_len_align = false,
-					max_len_align_padding = 1,
-					right_align = false,
-					right_align_padding = 7,
-					-- highlight = "Comment",
-					highlight = "NonText",
-				},
-				hover_actions = {
-					border = {
-						{ "╭", "FloatBorder" },
-						{ "─", "FloatBorder" },
-						{ "╮", "FloatBorder" },
-						{ "│", "FloatBorder" },
-						{ "╯", "FloatBorder" },
-						{ "─", "FloatBorder" },
-						{ "╰", "FloatBorder" },
-						{ "│", "FloatBorder" },
-					},
-					auto_focus = true,
-				},
-				dap = {
-					adapter = require("rustaceanvim.config").get_codelldb_adapter(codelldb_path, liblldb_path),
-				},
-			}
-		end,
-	},
-	{
-		"saecki/crates.nvim",
-		tag = "stable",
-		config = function()
-			require("crates").setup({
-				null_ls = {
-					enabled = true,
-					name = "crates.nvim",
-				},
-				popup = {
-					border = "rounded",
-				},
-			})
-		end,
-	},
+  {
+    "mrcjkb/rustaceanvim",
+    version = "6.9.1",
+    ft = { "rust" },
+    config = function()
+      local mason_registry = require("mason-registry")
+      vim.g.rustaceanvim = {
+        inlay_hints = {
+          auto = true,
+          only_current_line = false,
+          show_parameter_hints = true,
+          parameter_hints_prefix = "<-",
+          other_hints_prefix = "=> ",
+          max_len_align = false,
+          max_len_align_padding = 1,
+          right_align = false,
+          right_align_padding = 7,
+          highlight = "NonText",
+        },
+        hover_actions = {
+          border = {
+            { "╭", "FloatBorder" },
+            { "─", "FloatBorder" },
+            { "╮", "FloatBorder" },
+            { "│", "FloatBorder" },
+            { "╯", "FloatBorder" },
+            { "─", "FloatBorder" },
+            { "╰", "FloatBorder" },
+            { "│", "FloatBorder" },
+          },
+          auto_focus = true,
+        },
+      }
+    end,
+  },
+  {
+    "saecki/crates.nvim",
+    tag = "stable",
+    config = function()
+      require("crates").setup({
+        null_ls = {
+          enabled = true,
+          name = "crates.nvim",
+        },
+        popup = {
+          border = "rounded",
+        },
+      })
+    end,
+  },
 }
