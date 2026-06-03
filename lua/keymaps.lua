ee916eff4cb2d4864ca3761e777f901c3b8e5298
M.R. Siavash Katebzadeh <2138804+mrkatebzadeh@users.noreply.github.com>
2026-06-03 12:59:13 +0000

    Add new key mappings for window management and file tree

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,5 +1,6 @@
 local keymap = vim.keymap.set
 local s = { silent = true }
+local opts = { noremap = true, silent = true }
 
 vim.g.mapleader = " "
 
@@ -13,11 +14,21 @@ keymap("n", "k", function()
 end, { expr = true, silent = true }) -- Move up, but use 'gk' if no count is given
 keymap("n", "<C-d>", "<C-d>zz") -- Scroll down and center the cursor
 keymap("n", "<C-u>", "<C-u>zz") -- Scroll up and center the cursor
-keymap("n", "<Leader>w", "<cmd>w!<CR>", s) -- Save the current file
 keymap("n", "<Leader>q", "<cmd>q<CR>", s) -- Quit Neovim
 keymap("n", "<Leader>te", "<cmd>tabnew<CR>", s) -- Open a new tab
+keymap("n", "<Leader>ws", "<cmd>split<CR>", s) -- Open a horizontal split
 keymap("n", "<Leader>wv", "<cmd>vsplit<CR>", s) -- Split the window vertically
-keymap("n", "<Leader>wh", "<cmd>split<CR>", s) -- Split the window horizontally
+keymap("n", "<Leader>wh", "<C-w>h", s) -- Focus the left window
+keymap("n", "<Leader>wl", "<C-w>l", s) -- Focus the right window
+keymap("n", "<Leader>wj", "<C-w>j", s) -- Focus the window below
+keymap("n", "<Leader>wk", "<C-w>k", s) -- Focus the window above
+keymap("n", "<Leader>wd", "<cmd>close<CR>", s) -- Close the current window
+keymap("n", "<leader>w<Left>", "<C-w>h", s) -- Focus the left window via arrow key
+keymap("n", "<leader>w<Right>", "<C-w>l", s) -- Focus the right window via arrow key
+keymap("n", "<leader>w<Down>", "<C-w>j", s) -- Focus the window below via arrow key
+keymap("n", "<leader>w<Up>", "<C-w>k", s) -- Focus the window above via arrow key
+keymap("n", "<lt>", "<C-W>5>", opts) -- Shrink the current window width
+keymap("n", "<gt>", "<C-W>5<", opts) -- Widen the current window width
 keymap("n", "<Leader>lf", ":lua vim.lsp.buf.format()<CR>", s) -- Format the current buffer using LSP
 keymap("v", "<Leader>p", '"_dP') -- Paste without overwriting the default register
 keymap("x", "y", [["+y]], s) -- Yank to the system clipboard in visual mode
@@ -26,8 +37,8 @@ keymap("t", "<Esc>", "<C-\\><C-N>") -- Exit terminal mode
 keymap("n", "<leader>cd", '<cmd>lua vim.fn.chdir(vim.fn.expand("%:p:h"))<CR>')
 keymap("n", "<leader>pu", '<cmd>lua vim.pack.update()<CR>')
 
-local opts = { noremap = true, silent = true }
 keymap("n", "grd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts) -- Go to definition
-keymap("n", "<Leader>fe", "<cmd>Ex %:p:h<CR>") -- Open Netrw in the current file's directory
+keymap("n", "<Leader>fe", "<cmd>NvimTreeToggle<CR>", s) -- Toggle the file tree
+keymap("n", "<Leader>e", "<cmd>NvimTreeFindFile<CR>", s) -- Reveal current file in the tree
 keymap("n", "<leader>ff", '<cmd>FzfLua files<CR>')
 keymap("n", "<leader>fg", '<cmd>FzfLua live_grep<CR>')
