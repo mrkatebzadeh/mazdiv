dfc0b2de0f8b6701d653fa638c971a65a4aba23d
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2026-06-28 20:19:25 +0000

    Fixed treesitter

--- lua/plugins/editor/treesitter.lua
+++ lua/plugins/editor/treesitter.lua
@@ -20,35 +20,83 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
+		lazy = false,
 		build = ":TSUpdate",
-		event = { "BufReadPost", "BufNewFile" },
 		config = function()
-			local config = require("nvim-treesitter.configs")
-			config.setup({
-				ensure_installed = {
-					"c",
-					"lua",
-					"rust",
-					"vim",
-					"lua",
-					"html",
+			local ts = require("nvim-treesitter")
+			local has_tree_sitter_cli = vim.fn.executable("tree-sitter") == 1
+			if has_tree_sitter_cli then
+				ts.install({
 					"bash",
+					"c",
 					"dockerfile",
+					"html",
+					"lua",
 					"markdown",
 					"python",
+					"rust",
 					"toml",
-				},
-				auto_install = true,
-				highlight = { enable = true },
-				indent = { enable = true },
-				autotag = {
-					enable = true,
-				},
+					"vim",
+				})
+			end
+
+			local group = vim.api.nvim_create_augroup("MazdivTreesitter", { clear = true })
+			vim.api.nvim_create_autocmd("FileType", {
+				group = group,
+				callback = function(args)
+					local buf = args.buf
+					if vim.bo[buf].buftype ~= "" then
+						return
+					end
+
+					local ft = vim.bo[buf].filetype
+					local lang = vim.treesitter.language.get_lang(ft)
+					if not lang then
+						return
+					end
+
+					local ok = pcall(vim.treesitter.start, buf, lang)
+					if ok then
+						vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+						return
+					end
+
+					if not has_tree_sitter_cli then
+						return
+					end
+
+					-- Parser missing (or broken): try installing, then enable treesitter for this buffer.
+					local task = ts.install(lang)
+					if task and task.await then
+						task:await(function(err)
+							if err then
+								return
+							end
+							vim.schedule(function()
+								if not vim.api.nvim_buf_is_valid(buf) then
+									return
+								end
+								pcall(vim.treesitter.start, buf, lang)
+								vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+							end)
+						end)
+					end
+				end,
 			})
 		end,
 	},
 	{
 		"windwp/nvim-ts-autotag",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			require("nvim-ts-autotag").setup({
+				opts = {
+					enable_close = true,
+					enable_rename = true,
+					enable_close_on_slash = false,
+				},
+			})
+		end,
 	},
 }
 --[[ treesitter.lua ends here. ]]
